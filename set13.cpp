ebd563ffa91ff2b8739e723e6990eaf04eed13f5
king afsal <kingafsal87@gmail.com>
2018-09-11 22:06:12 +0530

    set 1-3

---
+++ set13.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char c;
+	cin>>c;
+	if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+	cout<<endl<<"Vowel";
+	else
+	cout<<endl<<"Consonant";
+	return 0;
+}
