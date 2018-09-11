331696aedb6728c7f3bab057812d71fcb1318b73
foreloner87 <34986630+foreloner87@users.noreply.github.com>
2018-09-11 22:50:37 +0530

    Update set13.cpp

--- set13.cpp
+++ set13.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
+#include<ctype.h>
 using namespace std;
 int main()
 {
 	char c;
 	cin>>c;
+	if(isalpha(c))
 	if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
 	cout<<endl<<"Vowel";
 	else
 	cout<<endl<<"Consonant";
+	else
+	cout<<"\nInvalid";
 	return 0;
 }
