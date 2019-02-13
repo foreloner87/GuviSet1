bcdae72661d08b3427ba748e467cf44301b13793
foreloner87 <34986630+foreloner87@users.noreply.github.com>
2019-02-13 12:36:27 +0530

    Update set3-28.py

--- set3-28.py
+++ set3-28.py
@@ -1,4 +1,7 @@
 n=int(input())
-l=list(map(int,input().split()))
-for i in range(len(l)):
-    print(l[i],i)
+if(n!=0):
+    l=list(map(int,input().split()))
+    for i in range(len(l)):
+        print(l[i],i)
+else:
+    print("Invalid input")
