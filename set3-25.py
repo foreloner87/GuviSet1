8246dac2a36053b3e0a01eb0f3a77fa2381a5ebf
foreloner87 <34986630+foreloner87@users.noreply.github.com>
2019-02-13 12:31:49 +0530

    Update set3-25.py

--- set3-25.py
+++ set3-25.py
@@ -1,4 +1,7 @@
 n=int(input())
-l=list(map(int,input().split()))
-l=sorted(l)
-print(l[(len(l)//2)])
+if(n!=0):
+  l=list(map(int,input().split()))
+  l=sorted(l)
+  print(l[(len(l)//2)])
+else:
+  print("Invalid input")
